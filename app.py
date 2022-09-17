8b98703e977806cf76cf0f086316c4193fc0f11a
parthahere001 <parthahere001.com>
2022-09-17 22:59:15 +0530

    Configured the app for POST requests

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 from flask import Flask
-
+from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
+
+
 
 @app.route('/')
 def hello_world():
         return "Hello World"
+
+
+@app.route('/sms', methods=['POST'])
+def sms():
+        messg="Hello There"
+        res = MessagingResponse()
+        res.message(messg)
+        return str(res)
 
 if __name__=='__main__':
     app.run()
